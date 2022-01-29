82632d566a226d9ebf0936d0fff5edbc3cb0d999
takahiro.tominaga <tt.300607@gmail.com>
2022-01-30 04:06:38 +0900

    presenterのDBに時間を読み書きする処理の単体テストを追加

---
+++ app/src/test/java/jp/mydns/kokoichi0206/countdowntimer/module/main/view/MockMainView.kt
@@ -0,0 +1,52 @@
+package jp.mydns.kokoichi0206.countdowntimer.module.main.view
+
+import android.content.Context
+import jp.mydns.kokoichi0206.countdowntimer.module.main.contract.MainContract
+import jp.mydns.kokoichi0206.util.MethodCallCounter
+import jp.mydns.kokoichi0206.viper.Presenter
+import java.time.LocalDateTime
+
+class MockMainView(
+    private val counter: MethodCallCounter = MethodCallCounter()
+) : MainContract.View {
+
+    var startedAt: LocalDateTime? = null
+    var deadLine: LocalDateTime? = null
+
+    enum class MockedMethod {
+        SET_MAIN_VIEW,
+        SET_MAIN_VIEW_WITH_TIME,
+        ON_DISASSEMBLE,
+    }
+
+    override fun setMainView() {
+        counter.increment(MockedMethod.SET_MAIN_VIEW.name)
+    }
+
+    override fun setMainViewWithTime(startedAt: LocalDateTime, deadLine: LocalDateTime) {
+        counter.increment(MockedMethod.SET_MAIN_VIEW_WITH_TIME.name)
+        this.startedAt = startedAt
+        this.deadLine = deadLine
+    }
+
+    override fun onDisassemble() {
+        counter.increment(MockedMethod.ON_DISASSEMBLE.name)
+    }
+
+    override fun beginAssembleModules(context: Context): Presenter {
+        TODO("Not yet implemented")
+    }
+
+    override fun beginDisassembleModules() {
+    }
+
+    fun clearMock() {
+        counter.clear()
+        startedAt = null
+        deadLine = null
+    }
+
+    fun getCount(method: MockedMethod): Int {
+        return counter.getCount(method.name)
+    }
+}
