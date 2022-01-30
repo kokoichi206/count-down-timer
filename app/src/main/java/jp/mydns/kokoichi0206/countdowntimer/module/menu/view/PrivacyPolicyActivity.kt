ae44c2931ae4b72979cb729fa6e3a0b0c6ae1739
takahiro.tominaga <tt.300607@gmail.com>
2022-01-31 05:50:23 +0900

    プラポリのアクティビティを追加 (#14)

---
+++ app/src/main/java/jp/mydns/kokoichi0206/countdowntimer/module/menu/view/PrivacyPolicyActivity.kt
@@ -0,0 +1,42 @@
+package jp.mydns.kokoichi0206.countdowntimer.module.menu.view
+
+import android.content.Context
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+import jp.mydns.kokoichi0206.countdowntimer.R
+import jp.mydns.kokoichi0206.countdowntimer.ui.theme.CountDownTimerTheme
+
+class PrivacyPolicyActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CountDownTimerTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    PrivacyPolicy(this)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PrivacyPolicy(context: Context) {
+    AndroidView(factory = {
+        WebView(context).apply {
+            webViewClient = WebViewClient()
+            loadDataWithBaseURL(null, getPrivacyPolicyString(context), "text/HTML", "UTF-8", null)
+        }
+    })
+}
+
+fun getPrivacyPolicyString(context: Context): String {
+    val asset = context.resources.openRawResource(R.raw.privacy_policy)
+    return asset.bufferedReader().use { it.readText() }
+}
